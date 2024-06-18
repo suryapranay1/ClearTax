e6d96ba5a0b274b5e4044d39f3dcb8384bb2a932
Surya Pranay <suryapranay2020@gmail.com>
2024-06-18 20:40:13 +0530

    developed upto designed section

--- src/components/designed.js
+++ src/components/designed.js
@@ -1,6 +1,6 @@
 import { Typography } from "@mui/material";
 import React from "react";
-import './designed.css';
+import "./designed.css";
 
 const Designed = () => {
   return (
@@ -8,27 +8,36 @@ const Designed = () => {
       <div className="video1">
         <video
           src="./img/1608208496_desktop_fold_designedbyexperts_lhs422.mp4"
-          controls autoPlay='true'
+          controls
+          autoPlay={true}
         ></video>
       </div>
       <div className="designedflex">
-      <div className="designed text">
-        <Typography sx={{ color: "#314259",fontSize:'56px',fontWeight:'700',ml:'1em' }}>Designed by experts</Typography>
-      </div>
-      
-        <Typography sx={{ color: '#9092A3',fontSize:'20px',fontWeight:'500',ml:'15em'}}>
-        <div className="designedSubtext">
-        <p>With our solutions, you don’t have to worry</p>
-<p>about complying with the latest regulations.</p>
-<p>60+ in-house tax experts, co-build and test</p>
-<p>every product feature. Over 60,000 tax experts</p>
-<p>give us constant feedback which helps us stay</p>
-<p>on top of every new tax clause.</p>
-</div>
+        <Typography
+          sx={{
+            color: "#314259",
+            fontSize: "56px",
+            fontWeight: "700",
+            ml: "1em",
+            mb: ".5em"
+          }}
+        >
+          Designed by experts
         </Typography>
-      
+        <Typography
+          sx={{
+            color: "#9092A3",
+            fontSize: "20px",
+            fontWeight: "500",
+            ml: "3em",
+          }}
+        >
+          With our solutions, you don’t have to worry <br></br>about complying with the
+          latest regulations.<br></br> 60+ in-house tax experts, co-build and test<br></br> every
+          product feature. Over 60,000 tax experts<br></br> give us constant feedback which
+          helps us stay<br></br> on top of every new tax clause.
+        </Typography>
       </div>
-      
     </div>
   );
 };
