92d33aced1cf9697f1db9b3b7903cbedc762fe02
Surya Pranay <suryapranay2020@gmail.com>
2024-06-18 20:53:29 +0530

    developed upto commited section

--- src/components/commited.js
+++ src/components/commited.js
@@ -1,21 +1,23 @@
 import { Typography } from "@mui/material";
 import React from "react";
-import './commited.css'
+import './commited.css';
+
 const Commited = () => {
   return (
     <div className="commited">
       <div className="commitedSection">
         <div className="commitedText">
-            <h2>Commited
-            <br></br>
-            to security</h2>
-            
+          <Typography sx={{fontSize:'3.5em',fontWeight:'700',color:'#314259',marginBottom:'.5em'}}>
+            Commited <br /> to security
+          </Typography> 
         </div>
         <div className="commitedParagraph">
-          <p>We value your data as if it were ours. All our<br></br>
-          certifications and licenses, including GSP,<br></br>
-          were awarded after rigorous scrutiny of our<br></br>
-          systems and multiple VAPT audits.</p>
+          <Typography sx={{color:'#9092A3',fontSize:'19px',fontWeight:'500',textAlign:'left'}}>
+            We value your data as if it were ours. All our<br />
+            certifications and licenses, including GSP,<br />
+            were awarded after rigorous scrutiny of our<br />
+            systems and multiple VAPT audits
+          </Typography>
         </div>
       </div>
 
@@ -26,7 +28,7 @@ const Commited = () => {
           height="200"
           loading="lazy"
           src="https://assets1.cleartax-cdn.com/cleartax/images/1620036013_security_certificates.png"
-        ></img>
+        />
       </div>
     </div>
   );
